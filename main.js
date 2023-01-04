4293260d96ec36ff7d11fe1955f28616f5ed6ef8
Khedr Ibrahim <108041341+KhedrIbrahim@users.noreply.github.com>
2023-01-04 09:36:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+// Switch Challenge: Change If code to Switch Code with Same Results. 
+let job = "Manager";
+let salary = 0;
+/*
+if (job === "Manager") {
+    salary = 8000;
+} else if (job === "IT" || job === "Support") {
+    salary = 6000;
+} else if (job === "Developer" || job === "Designer") {
+    salary = 7000;
+} else {
+    salary = 4000;
+}
+*/
+
+// Switch Code
+switch (job) {
+    case "Manager":
+    salary = 8000;
+    break;
+    case "IT":
+    case "Support":
+    salary = 6000;
+    break;
+    case "Developer":
+    case "Designer":
+    salary = 7000;
+    break;
+    default :
+    salary = 4000;
+    break;
+}
+console.log(salary);
+
+
+
+//If Challenge to chane the code ( Switch => If) With Same Results.
+
+let holidays = 4;
+let money = 0;
+
+/*
+switch (holidays) {
+    case 0:
+    money = 5000;
+    console.log(`My Money is ${money}`);
+    break;
+    case 1:
+    case 2:
+    money = 3000;
+    console.log(`My Money is ${money}`);
+    break;
+    case 3:
+    money = 2000;
+    console.log(`My Money is ${money}`);
+    break;
+    case 4:
+    money = 1000;
+    console.log(`My Money is ${money}`);
+    break;
+    case 5:
+    money = 0;
+    console.log(`My Money is ${money}`);
+    break;
+    default:
+    money = 0;
+    console.log(`My Money is ${money}`);
+}
+*/
+
+
+
+// If Code
+if (holidays === 0) {
+    money = 5000;
+} else if (holidays === 1 || holidays === 2 ) {
+    money = 3000;
+}else if (holidays === 3) {
+    money = 2000
+} else if (holidays === 4 ) {
+    money = 1000;
+}else if (holidays === 5 ) {
+    money = 0; 
+}else{
+    money = 0;
+}
+
+console.log(`My Money is ${money}`);
